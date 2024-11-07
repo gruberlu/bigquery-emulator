29d50e537294a980b3edd105f233fb009550e7b8
Lukas Gruber <72391764+gruberlu@users.noreply.github.com>
2024-11-07 09:38:57 +0100

    fix timestamps in nested structs and arrays

--- internal/types/types.go
+++ internal/types/types.go
@@ -2,6 +2,7 @@ package types
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/apache/arrow/go/v10/arrow/array"
@@ -184,24 +185,81 @@ func (c *TableCell) AppendValueToARROWBuilder(builder array.Builder) error {
 	}
 }
 
+func formatStructArray(cell *TableCell, schema []*bigqueryv2.TableFieldSchema) {
+	v, ok := cell.V.([]*TableCell)
+	for idx, c := range v {
+		if !ok {
+			return
+		}
+		r := c.V.(TableRow)
+		v[idx].V = TableRow{F: formatRow(r.F, schema)}
+	}
+}
+
+func formatStruct(cell *TableCell, schema []*bigqueryv2.TableFieldSchema) {
+	v, ok := cell.V.(TableRow)
+	if !ok {
+		return
+	}
+	f := formatRow(v.F, schema)
+	cell.V = TableRow{F: f}
+}
+
+func formatTimestampArray(cell *TableCell) {
+	v, ok := cell.V.([]*TableCell)
+	if !ok {
+		return
+	}
+	for idx, c := range v {
+		t, _ := zetasqlite.TimeFromTimestampValue(c.V.(string))
+		microsec := t.UnixNano() / int64(time.Microsecond)
+		v[idx] = &TableCell{
+			V: fmt.Sprint(microsec),
+		}
+	}
+}
+
+func formatTimestamp(cell *TableCell, schema *bigqueryv2.TableFieldSchema) {
+	str := cell.V.(string)
+	var t time.Time
+	// this should be fixed in the zetasqlite package
+	if _, err := strconv.ParseFloat(str, 64); err != nil {
+		t, _ = time.Parse("2006-01-02T15:04:05-07:00", str)
+	} else {
+		t, _ = zetasqlite.TimeFromTimestampValue(str)
+	}
+	microsec := t.UnixNano() / int64(time.Microsecond)
+	*cell = TableCell{
+		V:    fmt.Sprint(microsec),
+		Name: schema.Name,
+	}
+}
+
+func formatRow(cells []*TableCell, schema []*bigqueryv2.TableFieldSchema) []*TableCell {
+	formattedCells := make([]*TableCell, 0, len(cells))
+	for colIdx, cell := range cells {
+		switch {
+		case schema[colIdx].Type == string(types.RECORD) && schema[colIdx].Mode == string(types.RepeatedMode) && cell.V != nil:
+			formatStructArray(cell, schema[colIdx].Fields)
+		case schema[colIdx].Type == string(types.RECORD) && cell.V != nil:
+			formatStruct(cell, schema[colIdx].Fields)
+		case schema[colIdx].Type == string(types.TIMESTAMP) && schema[colIdx].Mode == string(types.RepeatedMode) && cell.V != nil:
+			formatTimestampArray(cell)
+		case schema[colIdx].Type == string(types.TIMESTAMP) && cell.V != nil:
+			formatTimestamp(cell, schema[colIdx])
+		}
+		formattedCells = append(formattedCells, cell)
+	}
+	return formattedCells
+}
+
 func Format(schema *bigqueryv2.TableSchema, rows []*TableRow, useInt64Timestamp bool) []*TableRow {
 	if !useInt64Timestamp {
 		return rows
 	}
 	formattedRows := make([]*TableRow, 0, len(rows))
 	for _, row := range rows {
-		cells := make([]*TableCell, 0, len(row.F))
-		for colIdx, cell := range row.F {
-			if schema.Fields[colIdx].Type == "TIMESTAMP" && cell.V != nil {
-				t, _ := zetasqlite.TimeFromTimestampValue(cell.V.(string))
-				microsec := t.UnixNano() / int64(time.Microsecond)
-				cells = append(cells, &TableCell{
-					V: fmt.Sprint(microsec),
-				})
-			} else {
-				cells = append(cells, cell)
-			}
-		}
+		cells := formatRow(row.F, schema.Fields)
 		formattedRows = append(formattedRows, &TableRow{
 			F: cells,
 		})
